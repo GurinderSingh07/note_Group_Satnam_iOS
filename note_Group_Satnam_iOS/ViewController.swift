2ac4655de9f365d5459d620b79b60c6def803aa0
Gurinder Singh <manivirk07@gmail.com>
2021-05-26 20:29:48 +0530

    Initial Commit

---
+++ note_Group_Satnam_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  note_Group_Satnam_iOS
+//
+//  Created by Gurinder Singh on 26/05/21.
+//  Copyright © 2021 Gurinder Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
