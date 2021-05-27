422c156b6020f6726da97e8f69b1db3d8b0557f2
Gurinder Singh <manivirk07@gmail.com>
2021-05-28 01:46:05 +0530

    Create IBOutlets of UIView present in the create note and set corner radius, border color and border width of all the respective UIViews.

--- note_Group_Satnam_iOS/ViewControllers/CreateNoteController.swift
+++ note_Group_Satnam_iOS/ViewControllers/CreateNoteController.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class CreateNoteController: UIViewController {
 
     //MARK:- IBOutlets
+    @IBOutlet weak var viewTitle: UIView!
+    @IBOutlet weak var viewDescription: UIView!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var viewSlideBar: UIView!
+    
+    @IBOutlet weak var detailField: UITextView!
+    
     
     //MARK:- ViewLifeCycle
     override func viewDidLoad() {
@@ -23,6 +34,30 @@ class CreateNoteController: UIViewController {
     func setupInitials(){
         
         self.navigationController?.toolbar.isHidden = true
+        
+        viewTitle.layer.cornerRadius = 10
+        viewTitle.layer.borderWidth = 1
+        viewTitle.layer.borderColor = UIColor.black.cgColor
+        
+//        viewDescription.layer.cornerRadius = 10
+//        viewDescription.layer.borderWidth = 1
+//        viewDescription.layer.borderColor = UIColor.black.cgColor
+        
+        mapView.layer.cornerRadius = 10
+        mapView.layer.borderWidth = 1
+        mapView.layer.borderColor = UIColor.black.cgColor
+        
+        imageView.layer.cornerRadius = 10
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.black.cgColor
+        
+        viewSlideBar.layer.cornerRadius = 10
+        viewSlideBar.layer.borderWidth = 1
+        viewSlideBar.layer.borderColor = UIColor.black.cgColor
+        
+        detailField.layer.cornerRadius = 10
+        detailField.layer.borderWidth = 1
+        detailField.layer.borderColor = UIColor.black.cgColor
     }
     
     //MARK:- UIButtons
